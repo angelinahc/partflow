eb7f805ba7d78a47a3a79bedc374527488f1bafc
aangelainis <angelinahenriquechaves@gmail.com>
2025-07-27 14:49:08 -0300

    First commit with the backend base

---
+++ api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/Teste-inicial", () =>
+    {
+        return "Tudo certo pra começar!";
+    }
+)
+.WithOpenApi();
+
+app.Run();
